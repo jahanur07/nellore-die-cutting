63552fad74fdde1869a883386de737751d060afb
Jahanur Raman <jahanurraman3@gmail.com>
2026-08-03 12:44:16 +0530

    Update project

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://abc-xyz.trycloudflare.com",
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: "http://127.0.0.1:8000/api",
 });
 
 api.interceptors.request.use(
